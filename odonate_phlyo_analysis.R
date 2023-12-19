635689d31cda0dcad59575b9529e6e6566f33888
isaac-finkelstein <Isaacfinkelstein2@gmail.com>
2023-12-19 15:10:15 -0500

    plotting territoriality on a tree not working

--- odonate_phlyo_analysis.R
+++ odonate_phlyo_analysis.R
@@ -12,10 +12,22 @@ load(file="data/Odo.tree.Waller.Svensson.2017.rda") #odonate tree extracated fro
 
 #Put data into a readable dataframe -- one species per row
 terr_table<- ftable(my_data$Formatted_species, my_data$Territorial) #this has 3 rows
-prop_terr<- round(terr_table[,3]/(terr_table[,2]+ terr_table[,3]),3) # so this calculates the percentage "yes" for territorial
+prop_terr<- round(terr_table[,3]/(terr_table[,2]+ terr_table[,3]),0) # so this calculates the percentage "yes" for territorial
+#note that I am rounding to 1 whole number- so 0.5-> 1. Figure out how to set this to a 66% threshold later
 sn<- attr(terr_table,"row.vars")[[1]]
 terr_data_with_na<- data.frame(sn,prop_terr) #so this is a dataframe with the proporitons of territorial "yes" for each species
 terr_data<- terr_data_with_na[complete.cases(terr_data_with_na), ] #removed NA values 
+
+#to make the terriotrial variable binary (yes or no) make it a factor
+prop_terr_factor <- factor(ifelse(prop_terr == 1, "yes", "no"))
+terr_data_with_na_factor <- data.frame(sn, prop_terr_factor = prop_terr_factor)
+terr_data_factor <- terr_data_with_na_factor[complete.cases(terr_data_with_na_factor), ]
+
+# Create a data frame
+terr_data_with_na <- data.frame(sn, prop_terr = prop_terr_factor)
+
+# Remove NA values
+terr_data_factor <- terr_data_with_na[complete.cases(terr_data_with_na), ]
 
 #prune tree to match data
 chk<-name.check(tree, terr_data, data.names=as.character(terr_data$sn))
@@ -24,7 +36,7 @@ odonate_tree<-drop.tip(tree, chk$tree_not_data) #dropped tree_not_data species
 odonate_tree
 # Identify species to drop from terr_data
 species_to_drop <- chk$data_not_tree
-odonate_terr_data <- terr_data[!(terr_data$sn %in% species_to_drop), ] #droopped data_not_tree species from dataset
+odonate_terr_data <- terr_data[!(terr_data$sn %in% species_to_drop), ] #dropped data_not_tree species from dataset
 name.check(odonate_tree, odonate_terr_data, data.names=as.character(odonate_terr_data$sn))
 
 #let's plot the tree
@@ -41,3 +53,20 @@ plot(anisoptera_tree, type="fan", cex=0.5, ftype="i")
 zygoptera_tree_extract<-unname(ape::extract.clade(odonate_tree, node = 584)$tip.label)
 zygoptera_tree<-drop.tip(odonate_tree, zygoptera_tree_extract)
 plot(zygoptera_tree, type="fan", cex=0.5, ftype="i")
+
+#let's plot territoriality on my tree
+#I want to use the factor (binary yes vs no)
+odonate_tree_factor<-drop.tip(tree, chk$tree_not_data) #dropped tree_not_data species
+odonate_tree_factor
+species_to_drop <- chk$data_not_tree
+odonate_terr_data_factor <- terr_data_factor[!(terr_data_factor$sn %in% species_to_drop), ] #dropped data_not_tree species from dataset
+name.check(odonate_tree, odonate_terr_data_factor, data.names=as.character(odonate_terr_data_factor$sn))
+
+
+#let's plot territoriality on my tree
+terr_mode<-setNames(odonate_terr_data_factor[,2], rownames(odonate_terr_data_factor))
+cols<-setNames(c("lightblue", "red"), levels(terr_mode))
+plotTree.datamatrix(odonate_tree_factor, as.data.frame(terr_mode),
+                    colours= list(cols), header=FALSE, fsize=0.45)
+legend("topright", legend=levels(terr_mode), pch=22, pt.cex=1.5, pt.bg=cols, bty="n", cex=0.8)
+#hmmmm not working. 
