00873a33fdf8febe5a342c78cb231ac982e87fa9
isaac-finkelstein <Isaacfinkelstein2@gmail.com>
2023-12-20 16:02:45 -0500

    I got closer on the tree - but the colours are wrong!

--- odonate_phlyo_analysis.R
+++ odonate_phlyo_analysis.R
@@ -64,9 +64,31 @@ name.check(odonate_tree, odonate_terr_data_factor, data.names=as.character(odona
 
 
 #let's plot territoriality on my tree
-terr_mode<-setNames(odonate_terr_data_factor[,2], rownames(odonate_terr_data_factor))
+terr_mode<-setNames(odonate_terr_data_factor$prop_terr, odonate_terr_data_factor$sn)
 cols<-setNames(c("lightblue", "red"), levels(terr_mode))
 plotTree.datamatrix(odonate_tree_factor, as.data.frame(terr_mode),
                     colours= list(cols), header=FALSE, fsize=0.45)
 legend("topright", legend=levels(terr_mode), pch=22, pt.cex=1.5, pt.bg=cols, bty="n", cex=0.8)
 #hmmmm not working. 
+
+#I'm going to try for just Anisoptera
+#Plotting territoriality on Anisoptera tree
+#prune tree to match data
+chk_anis<-name.check(anisoptera_tree, terr_data_factor, data.names=as.character(terr_data_factor$sn))
+summary(chk_anis)
+anis_tree<-drop.tip(anisoptera_tree, chk_anis$tree_not_data) #dropped tree_not_data species
+anis_tree
+# Identify species to drop from terr_data - using the factor "Yes or No"
+species_to_drop_anis <- chk_anis$data_not_tree
+anis_terr_data_factor <- terr_data_factor[!(terr_data_factor$sn %in% species_to_drop_anis), ] #dropped data_not_tree species from dataset
+name.check(anis_tree, anis_terr_data_factor, data.names=as.character(anis_terr_data_factor$sn))
+
+#plot anisoptera tree
+anis_terr_mode <- setNames(anis_terr_data_factor$prop_terr, anis_terr_data_factor$sn)
+cols<-setNames(c("lightblue", "red"), levels(anis_terr_mode))
+plotTree.datamatrix(anis_tree, as.data.frame(anis_terr_mode),
+                    colours= list(cols), header=FALSE, fsize=0.45)
+legend("topright", legend=levels(anis_terr_mode), pch=22, pt.cex=1.5, pt.bg=cols, bty="n", cex=0.8)
+
+#this may be working but the colours are wrong - plus I can't read the species names to check if it's right. 
+#the difference is in the setNames function line -- how I made my anis_terr_mode
