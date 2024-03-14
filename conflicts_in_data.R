42afb2b0916b8663af97c3e1ce3fcbfd14bd54f1
isaac-finkelstein <isaacfinkelstein2@gmail.com>
2024-03-14 09:33:51 -0400

    compared the two trees

--- conflicts_in_data.R
+++ conflicts_in_data.R
@@ -1,10 +1,18 @@
 #finding conflicts in the data
+library(phytools)
+library(dplyr)
+library(geiger)
 
 my_data<- read.csv("data/data_v4.csv") #this dataset (3rd version) switches "tandem" for "contact" in De Recende's data.
 #For some reason, they use both terms. But since they mean the same thing, I changed them all to "Contact"
 #I also emailed to ask, but did not get a response. 
 load(file="data/Odo.tree.Waller.Svensson.2017.rda") #odonate tree extracated from Waller and Svensson 2017
 #str(tree) #plot(tree, no.margin=TRUE) #tree$Nnode #tree$tip.label -- to check the structure of the tree
+
+#tree from Rocha-Ortega et al., 2020 in Proceedings
+#it uses the same structure as Waller and Svensson 2017 but has different species coverage.
+rocha_tree<-read.nexus(file="data/Rocha_ortega_tree.nex")
+#I compare the trees starting at line 167
 
 #total number of species in dataset
 total_species <- length(unique(my_data$Formatted_species))
@@ -14,7 +22,7 @@ total_species <- length(unique(my_data$Formatted_species))
 terr_table<- ftable(my_data$Formatted_species, my_data$Territorial) #this has 3 columns, the first column is for NAs
 prop_terr<- round(terr_table[,3]/(terr_table[,2]+ terr_table[,3]),2) # so this calculates the percentage "yes" for territorial
 sn<- attr(terr_table,"row.vars")[[1]]
-terr_data_with_na<- data.frame(sn,sp_terr) #so this is a dataframe with territorial (1/0) for each species
+terr_data_with_na<- data.frame(sn,prop_terr) #so this is a dataframe with territorial (1/0) for each species
 terr_data<- terr_data_with_na[complete.cases(terr_data_with_na), ] #removed NA values 
 str(terr_data)
 #total number of species = 643
@@ -153,3 +161,98 @@ colnames(conflict_lot_size)<-c("Species", "Conflict")
 conflict_lotic_size<-conflict_lot_size[conflict_lot_size$Conflict==5,]
 conflict_lotic_size<-na.omit(conflict_lotic_size)
 str(conflict_lotic_size) #1 conflict
+
+
+
+
+#comparing trees
+
+#start with Waller and Svensson tree
+#prune tree to match data
+chk<-name.check(tree, terr_data, data.names=as.character(terr_data$sn))
+summary(chk)
+odonate_tree<-drop.tip(tree, chk$tree_not_data) #dropped tree_not_data species
+odonate_tree
+# Identify species to drop from terr_data
+species_to_drop <- chk$data_not_tree
+odonate_terr_data <- terr_data[!(terr_data$sn %in% species_to_drop), ] #dropped data_not_tree species from dataset
+name.check(odonate_tree, odonate_terr_data, data.names=as.character(odonate_terr_data$sn))
+
+#let's plot the entire tree - busy
+plot(odonate_tree, type="fan", cex=0.5, fsize=0.6, ftype="i") #it's very busy because it has 372 tips
+#let's plot territoriality on my tree
+
+
+#plot territoriality on tree
+#to make the territorial variable binary (yes or no) make it a factor
+prop_terr_factor <- factor(ifelse(prop_terr == 1, "yes", "no"))
+terr_data_with_na_factor <- data.frame(sn, prop_terr_factor = prop_terr_factor) # Create a data frame
+terr_data_factor <- terr_data_with_na_factor[complete.cases(terr_data_with_na_factor), ] #Removes NA values
+
+odonate_tree_factor<-drop.tip(tree, chk$tree_not_data) #dropped tree_not_data species
+odonate_tree_factor
+species_to_drop <- chk$data_not_tree
+odonate_terr_data_factor <- terr_data_factor[!(terr_data_factor$sn %in% species_to_drop), ] #dropped data_not_tree species from dataset
+name.check(odonate_tree, odonate_terr_data_factor, data.names=as.character(odonate_terr_data_factor$sn))
+
+
+terr_mode<-setNames(odonate_terr_data_factor$prop_terr, odonate_terr_data_factor$sn)
+cols<-setNames(c("forestgreen", "goldenrod1"), levels(terr_mode))
+plotTree.datamatrix(odonate_tree_factor, as.data.frame(terr_mode),
+                    colours= list(cols), header=FALSE)
+legend("topright", legend=levels(terr_mode), pch=22, pt.cex=1.5, pt.bg=cols, bty="n", cex=0.8)
+
+
+#Now let's do the same with the Rocha-Ortega tree
+#prune tree to match data
+chk_rocha<-name.check(rocha_tree, terr_data, data.names=as.character(terr_data$sn))
+summary(chk_rocha)
+odonate_tree_rocha<-drop.tip(rocha_tree, chk_rocha$tree_not_data) #dropped tree_not_data species
+odonate_tree_rocha
+# Identify species to drop from terr_data
+species_to_drop_rocha <- chk_rocha$data_not_tree
+odonate_terr_data_rocha <- terr_data[!(terr_data$sn %in% species_to_drop_rocha), ] #dropped data_not_tree species from dataset
+name.check(odonate_tree_rocha, odonate_terr_data_rocha, data.names=as.character(odonate_terr_data_rocha$sn))
+
+#let's plot the entire tree - busy
+plot(odonate_tree_rocha, type="fan", cex=0.5, fsize=0.6, ftype="i") #it's very busy because it has 372 tips
+#let's plot territoriality on my tree
+
+
+#plot territoriality on tree
+#to make the territorial variable binary (yes or no) make it a factor
+prop_terr_factor <- factor(ifelse(prop_terr == 1, "yes", "no"))
+terr_data_with_na_factor <- data.frame(sn, prop_terr_factor = prop_terr_factor) # Create a data frame
+terr_data_factor <- terr_data_with_na_factor[complete.cases(terr_data_with_na_factor), ] #Removes NA values
+
+odonate_tree_factor_rocha<-drop.tip(rocha_tree, chk_rocha$tree_not_data) #dropped tree_not_data species
+odonate_tree_factor_rocha
+species_to_drop_rocha <- chk_rocha$data_not_tree
+odonate_terr_data_factor_rocha <- terr_data_factor[!(terr_data_factor$sn %in% species_to_drop_rocha), ] #dropped data_not_tree species from dataset
+name.check(odonate_tree_rocha, odonate_terr_data_factor_rocha, data.names=as.character(odonate_terr_data_factor_rocha$sn))
+
+
+terr_mode<-setNames(odonate_terr_data_factor_rocha$prop_terr, odonate_terr_data_factor_rocha$sn)
+cols<-setNames(c("forestgreen", "goldenrod1"), levels(terr_mode))
+plotTree.datamatrix(odonate_tree_factor_rocha, as.data.frame(terr_mode),
+                    colours= list(cols), header=FALSE)
+legend("topright", legend=levels(terr_mode), pch=22, pt.cex=1.5, pt.bg=cols, bty="n", cex=0.8)
+
+str(odonate_terr_data_factor_rocha)
+#vs
+str(odonate_terr_data_factor)
+#much fewer species on the Rocha_tree
+
+#what species differ?
+species_rocha <- odonate_terr_data_factor_rocha$sn
+species_sven <- odonate_terr_data_factor$sn
+
+#unique species to each dataset
+species_unique_to_rocha <- setdiff(species_rocha, species_sven)
+species_unique_to_sven <- setdiff(species_sven, species_rocha)
+
+str(species_unique_to_rocha)
+str(species_unique_to_sven)
+
+#I can do the same thing for the specific traits wherer the results differ when using the different trees
+
