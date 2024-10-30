dc2e7fb325795252107f52f2cc2457cbefba58bf
isaac-finkelstein <isaacfinkelstein2@gmail.com>
2024-10-30 17:18:33 -0400

    Made some progress here.

--- Clean_Ancestral_state_recon_code.R
+++ Clean_Ancestral_state_recon_code.R
@@ -49,7 +49,7 @@ name.check(odonate_tree, odonate_terr_data, data.names=as.character(odonate_terr
 
 #plot the tree
 plot(odonate_tree, type="fan", cex=0.5, fsize=0.6, ftype="i")
-str(odonate_tree) #370 species/tips
+str(odonate_tree) #371 species/tips
 
 
 #Choose a discrete character model (rate-of-transition model)
@@ -58,6 +58,7 @@ str(odonate_tree) #370 species/tips
 #equal rates (ER)
 #all rates different (ARD)
 #two irreversible models: going from 0 -> 1,or from 1 -> 0, but not vice versa
+terr_mode<-setNames(terr_data_factor$prop_terr, terr_data_factor$sn)
 
 fit_er<-fitMk(odonate_tree, terr_mode, model = "ER")
 fit_ard<-fitMk(odonate_tree, terr_mode, model = "ARD")
@@ -88,3 +89,199 @@ plot(fit_ard, show.zeros=FALSE, mar=rep(0,4), signif=5)
 
 
 #Ancestral state reconstruction
+#using marginal ancestral state reconstruction as this is more popular
+#and provides uncertainty about specific values
+
+#create a special data frame of species names and trait data
+#character data must be numerical interger (0,1)
+odonate_data<-data.frame(Genus_sp=names(terr_mode), terr_mode=as.numeric(terr_mode)-1)
+head(odonate_data, n=10)
+
+#prune tree to match data
+chk<-name.check(tree, odonate_data, data.names=as.character(odonate_data$Genus_sp))
+summary(chk)
+odonate_tree<-drop.tip(tree, chk$tree_not_data) #dropped tree_not_data species
+odonate_tree
+# Identify species to drop
+species_to_drop <- chk$data_not_tree
+odonate_data <- odonate_data[!(odonate_data$Genus_sp%in% species_to_drop), ] #dropped data_not_tree species from dataset
+name.check(odonate_tree, odonate_data, data.names=as.character(odonate_data$Genus_sp))
+
+
+#fit model
+fit_marginal<- corHMM(odonate_tree, odonate_data, node.states = "marginal",
+                      rate.cat=1, rate.mat=NULL)
+fit_marginal
+head(fit_marginal$states) 
+
+#plot
+cols<-setNames(c("turquoise", "brown"), levels(terr_mode))
+#plot this
+plotTree.datamatrix(odonate_tree, as.data.frame(terr_mode),
+                    colors=list(cols), header=FALSE, fsize=0.45)
+legend("topright", legend=levels(terr_mode), pch=22, pt.cex=1.5, pt.bg=cols, bty="n", cex=0.8)
+nodelabels(pie=fit_marginal$states, piecol=cols, cex=0.3)
+tiplabels(pie=to.matrix(terr_mode, sort(unique(terr_mode))), piecol=cols, cex=0.3)
+
+#Ancestral state reconstruction using the ace function from the package "ape"
+fit_ARD_again<-ace(odonate_data$terr_mode, odonate_tree, model="ARD", type="discrete", marginal = TRUE)
+round(fit_ARD_again$lik.anc, 3)
+head(fit_ARD_again$lik.anc)
+plotTree(odonate_tree, fsize=0.5, ftype="i")
+nodelabels(node=1:odonate_tree$Nnode+Ntip(odonate_tree),
+           pie=fit_ARD_again$lik.anc, piecol = cols, cex=0.3)
+tiplabels(pie=to.matrix(terr_mode, sort(unique(terr_mode))), piecol=cols, cex=0.3)
+legend("topright", legend=levels(terr_mode), pch=22, pt.cex=1.5, pt.bg=cols, bty="n", cex=0.8)
+
+
+
+#Calculate phylogenetic signal
+
+#using the D statistic from Fritz & Purvis 2010
+
+#territoriality
+signal_terr<-phylo.d(odonate_terr_data, odonate_tree, names.col=sn, binvar=sp_terr)
+
+
+#for other binary traits I need to create a dataframe for each trait
+#active behaviour (flier/percher)
+binary_fly_v_perch<-ftable(my_data$Formatted_species, my_data$Flier.vs.percher)
+prop_fly_v_perch<-round(binary_fly_v_perch[,3]/(binary_fly_v_perch[,2]+binary_fly_v_perch[,3]),2) #this is proportion Percher
+#1=percher, 0 = flier
+#set a 75% threshold = 3:1 threshold
+sp_fly_v_perch<-ifelse(prop_fly_v_perch >= 0.75, 1, ifelse(prop_fly_v_perch <=0.25, 0, NA))
+sn<-attr(binary_fly_v_perch, "row.vars")[[1]]
+binary_fly_v_perch_df<-data.frame(sn, sp_fly_v_perch, stringsAsFactors=TRUE) #includes NA
+
+#courtship
+binary_courtship<-ftable(my_data$Formatted_species, my_data$Courtship)
+prop_court<-round(binary_courtship[,3]/(binary_courtship[,2]+binary_courtship[,3]),2) #this is proportion "Yes"
+#1=yes, 0 = no
+#set a 75% threshold = 3:1 threshold
+sp_courtship<-ifelse(prop_court>=0.75,1, ifelse(prop_court<=0.25, 0, NA))
+sn<-attr(binary_courtship, "row.vars")[[1]]
+binary_court_df<-data.frame(sn, sp_courtship, stringsAsFactors=TRUE) #includes NA
+
+
+#oviposition method (endophytic vs exophytic)
+#make all epiphytic = exophytic - to make it binary. 
+my_data_mutated <- my_data %>%
+  mutate(Oviposition.type..endophytic.vs.exophytic. = ifelse(Oviposition.type..endophytic.vs.exophytic. %in% c("Epiphytic", "Exophytic"), "Exophytic", Oviposition.type..endophytic.vs.exophytic.))
+# Convert the variable to a factor with specified levels
+my_data_mutated$Oviposition.type..endophytic.vs.exophytic. <- factor(
+  my_data_mutated$Oviposition.type..endophytic.vs.exophytic.,
+  levels = c("Endophytic", "Exophytic")
+)
+binary_ovi<-ftable(my_data_mutated$Formatted_species, my_data_mutated$Oviposition.type..endophytic.vs.exophytic.)
+prop_ovi<-round(binary_ovi[,2]/(binary_ovi[,1]+binary_ovi[,2]),2) #This is proportion exophytic
+#1=exophytic, 0=endophytic
+#set a 75% threshold = 3:1 threshold
+sp_ovi<-ifelse(prop_ovi >= 0.75, 1, ifelse(prop_ovi <=0.25, 0, NA))
+sn<-attr(binary_ovi, "row.vars")[[1]]
+binary_ovi_df<-data.frame(sn, sp_ovi, stringsAsFactors=TRUE) #includes NA
+
+#oviposition habitat (lotic/lentic)
+filtered_lo_len <- subset(my_data, Lotic.vs.lentic..breeding.habitat. %in% c("Lotic", "Lentic"))
+lo_len_var<-ftable(filtered_lo_len$Formatted_species, filtered_lo_len$Lotic.vs.lentic..breeding.habitat.)
+prop_lo_len<-round(lo_len_var[,2]/(lo_len_var[,1]+lo_len_var[,2]),2) #this is the proportion that is lotic
+#set a 75% threshold = 3:1 threshold
+sp_lo_len<-ifelse(prop_lo_len >= 0.75, 1, ifelse(prop_lo_len <=0.25, 0, NA))
+sn<-attr(lo_len_var, "row.vars")[[1]]
+binary_lo_len<-data.frame(sn, sp_lo_len, stringsAsFactors = TRUE) #includes NA
+
+
+#prune binary variables to match the tree
+chk_fly_v_perch<-name.check(tree, binary_fly_v_perch_df, data.names=as.character(binary_fly_v_perch_df$sn))
+tree_fly_v_perch <- drop.tip(tree, chk_fly_v_perch$tree_not_data)#dropped tree_not_data species
+#identify species to drop from data
+fly_v_perch_species_to_drop<-chk_fly_v_perch$data_not_tree
+fly_v_perch_data<-binary_fly_v_perch_df[!(binary_fly_v_perch_df$sn %in% fly_v_perch_species_to_drop),] #dropped data_not_tree species from dataset
+name.check(tree_fly_v_perch, fly_v_perch_data, data.names=as.character(fly_v_perch_data$sn))
+
+chk_court<-name.check(tree, binary_court_df, data.names=as.character(binary_court_df$sn))
+tree_court <- drop.tip(tree, chk_court$tree_not_data)#dropped tree_not_data species
+#identify species to drop from data
+court_species_to_drop<-chk_court$data_not_tree
+court_data<-binary_court_df[!(binary_court_df$sn %in% court_species_to_drop),] #dropped data_not_tree species from dataset
+name.check(tree_court, court_data, data.names=as.character(court_data$sn))
+
+chk_ovi<-name.check(tree, binary_ovi_df, data.names=as.character(binary_ovi_df$sn))
+tree_ovi <- drop.tip(tree, chk_ovi$tree_not_data)#dropped tree_not_data species
+#identify species to drop from data
+ovi_species_to_drop<-chk_ovi$data_not_tree
+ovi_data<-binary_ovi_df[!(binary_ovi_df$sn %in% ovi_species_to_drop),] #dropped data_not_tree species from dataset
+name.check(tree_ovi,ovi_data, data.names=as.character(ovi_data$sn))
+
+chk_lo_len<-name.check(tree, binary_lo_len, data.names=as.character(binary_lo_len$sn))
+tree_lo_len <- drop.tip(tree, chk_lo_len$tree_not_data)#dropped tree_not_data species
+#identify species to drop from data
+lo_len_species_to_drop<-chk_lo_len$data_not_tree
+lo_len_data<-binary_lo_len[!(binary_lo_len$sn %in% lo_len_species_to_drop),] #dropped data_not_tree species from dataset
+name.check(tree_lo_len, lo_len_data, data.names=as.character(lo_len_data$sn))
+
+
+#phylogenetic signal of binary traits
+signal_terr<-phylo.d(odonate_terr_data, odonate_tree, names.col=sn, binvar=sp_terr)
+signal_fly_v_perch<-phylo.d(fly_v_perch_data, tree_fly_v_perch, names.col=sn, binvar=sp_fly_v_perch)
+signal_courtship<-phylo.d(court_data, tree_court, names.col=sn, binvar=sp_courtship)
+signal_oviposition<-phylo.d(ovi_data, tree_ovi, names.col=sn, binvar = sp_ovi)
+signal_lo_len<-phylo.d(lo_len_data, tree_lo_len, names.col = sn, binvar = sp_lo_len)
+
+#phylogenetic signal for non-binary traits
+#calculating delta, a measure of phylogenetic signal
+
+#from Borges et al., 2019 github
+#https://github.com/mrborges23/delta_statistic
+source("code.R") #taken from https://github.com/mrborges23/delta_statistic/tree/master
+
+#all branches must be positive
+odonate_tree$edge.length[odonate_tree$edge.length==0] <-quantile(odonate_tree$edge.length, 0.1)*0.1
+
+
+#mate guarding
+my_data$Mate.guarding <- trimws(my_data$Mate.guarding)
+my_data_filtered <- my_data[!(my_data$Mate.guarding == "Both"), ] #There's only 1 "Both" so I'm removing it.
+mate_guarding_table<-ftable(my_data_filtered$Formatted_species, my_data_filtered$Mate.guarding)
+
+prop_no<-round(mate_guarding_table[,3]/(mate_guarding_table[,3]+mate_guarding_table[,4]+mate_guarding_table[,2]), 2)
+prop_contact<-round(mate_guarding_table[,2]/(mate_guarding_table[,3]+mate_guarding_table[,4]+mate_guarding_table[,2]), 2)
+prop_non_contact<-round(mate_guarding_table[,4]/(mate_guarding_table[,3]+mate_guarding_table[,4]+mate_guarding_table[,2]), 2)
+prop_mate_guard_df<-data.frame(prop_no=prop_no,
+                                  prop_contact=prop_contact,
+                                  prop_non_contact=prop_non_contact)
+prop_mate_guard_df<-data.frame(
+  prop_no=ifelse(prop_no>0.75,1,NA),
+  prop_contact=ifelse(prop_contact>0.75,1,NA),
+  prop_non_contact=ifelse(prop_non_contact>0.75, 1, NA))
+
+prop_mate_guard_df <- prop_mate_guard_df %>%
+  mutate(mate_guard = case_when(
+    prop_no == 1 ~ 0,         # 0 = no mate guarding
+    prop_contact == 1 ~ 2,     # 2 = contact mate guarding
+    prop_non_contact == 1 ~ 1, # 1 = non-contact mate guarding
+    TRUE ~ NA_real_            # NA if none of the conditions are met
+  ))
+prop_mate_guard_df <- prop_mate_guard_df[, !(names(prop_mate_guard_df) %in% c("prop_no", "prop_contact", "prop_non_contact"))]
+sn<-attr(mate_guarding_table, "row.vars")[[1]]
+mate_guard_df<-data.frame(sn, prop_mate_guard_df, stringsAsFactors=TRUE)
+mate_guard_df<- mate_guard_df[complete.cases(mate_guard_df), ] #remove NA values 
+
+#prune tree to match data
+chk<-name.check(tree, mate_guard_df, data.names=as.character(mate_guard_df$sn))
+summary(chk)
+odonate_tree<-drop.tip(tree, chk$tree_not_data) #dropped tree_not_data species
+odonate_tree
+# Identify species to drop from terr_data
+species_to_drop <- chk$data_not_tree
+mate_guard_data <- mate_guard_df[!(mate_guard_df$sn %in% species_to_drop), ] #dropped data_not_tree species from dataset
+name.check(odonate_tree, mate_guard_data, data.names=as.character(mate_guard_data$sn))
+
+#define the trait vector
+mate_guard_data$sn <- droplevels(mate_guard_data$sn) #drop unused levels in the species (sn) column
+order_in_mate_guard <- match(odonate_tree$tip.label, mate_guard_data$sn) #must be same order as tree
+mate_guard_data_reordered <- mate_guard_data[order_in_mate_guard, ] #tree and data are in the same order
+trait_with_na<-mate_guard_data_reordered$sn
+trait<- trait_with_na[!is.na(trait_with_na)] #removed NA values 
+
+#calculate delta
+deltaA<-delta(trait, odonate_tree, 0.1, 0.0589, 1000, 10, 100)
